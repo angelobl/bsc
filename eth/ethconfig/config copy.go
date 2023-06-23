ec5d25148de4fb42aee70df2160b897bafe1e6b7
angelo-ftf <angelo.bertello@fulltimeforce.com>
2023-06-22 22:46:37 -0500

    Add peer checking

--- eth/ethconfig/config copy.go
+++ eth/ethconfig/config copy.go
@@ -91,11 +91,11 @@ var Defaults = Config{
 		Recommit:      3 * time.Second,
 		DelayLeftOver: 50 * time.Millisecond,
 	},
-	TxPool:        core.DefaultTxPoolConfig,
-	RPCGasCap:     50000000,
-	RPCEVMTimeout: 5 * time.Second,
-	GPO:           FullNodeGPO,
-	RPCTxFeeCap:   1, // 1 ether
+	TxPool:            core.DefaultTxPoolConfig,
+	RPCGasCap:         50000000,
+	RPCEVMTimeout:     5 * time.Second,
+	GPO:               FullNodeGPO,
+	RPCTxFeeCap:       1, // 1 ether
 	MetadataCacheSize: 10,
 }
 
@@ -129,15 +129,8 @@ type Config struct {
 	Genesis *core.Genesis `toml:",omitempty"`
 
 	// Protocol options
-	NetworkId uint64 // Network ID to use for selecting peers to connect to
-	SyncMode  downloader.SyncMode
-
-	// DisablePeerTxBroadcast is an optional config and disabled by default, and usually you do not need it.
-	// When this flag is enabled, you are requesting remote peers to stop broadcasting new transactions to you, and
-	// it does not mean that your node will stop broadcasting transactions to remote peers.
-	// If your node does care about new mempool transactions (e.g., running rpc services without the need of mempool
-	// transactions) or is continuously under high pressure (e.g., mempool is always full), then you can consider
-	// to turn it on.
+	NetworkId              uint64 // Network ID to use for selecting peers to connect to
+	SyncMode               downloader.SyncMode
 	DisablePeerTxBroadcast bool
 	MetadataCacheSize      uint64
 
@@ -146,14 +139,12 @@ type Config struct {
 	EthDiscoveryURLs   []string
 	SnapDiscoveryURLs  []string
 	TrustDiscoveryURLs []string
-	BscDiscoveryURLs   []string
 
 	NoPruning           bool // Whether to disable pruning and flush everything to disk
 	DirectBroadcast     bool
 	DisableSnapProtocol bool //Whether disable snap protocol
 	DisableDiffProtocol bool //Whether disable diff protocol
 	EnableTrustProtocol bool //Whether enable trust protocol
-	DisableBscProtocol  bool //Whether disable bsc protocol
 	DiffSync            bool // Whether support diff sync
 	PipeCommit          bool
 	RangeLimit          bool
@@ -185,14 +176,7 @@ type Config struct {
 	DatabaseDiff       string
 	PersistDiff        bool
 	DiffBlock          uint64
-	// PruneAncientData is an optional config and disabled by default, and usually you do not need it.
-	// When this flag is enabled, only keep the latest 9w blocks' data, the older blocks' data will be
-	// pruned instead of being dumped to freezerdb, the pruned data includes CanonicalHash, Header, Block,
-	// Receipt and TotalDifficulty.
-	// Notice: the PruneAncientData once be turned on, the get/chaindata/ancient dir will be removed,
-	// if restart without the pruneancient flag, the ancient data will start with the previous point that
-	// the oldest unpruned block number.
-	PruneAncientData bool
+	PruneAncientData   bool
 
 	TrieCleanCache          int
 	TrieCleanCacheJournal   string        `toml:",omitempty"` // Disk journal directory for trie cache to survive node restarts
